b265d393851a356648000b2cc3fd609e49387d06
Anubhav <anubhav_a@me.iitr.ac.in>
2024-11-15 16:56:19 +0530

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     domains: ['images.unsplash.com', 'files.edgestore.dev'],
+    unoptimized: process.env.NODE_ENV === 'production',
+  },
+  distDir: '.next',
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    };
   },
 };
 
