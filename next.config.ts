648546638e7d69a7cf896d04bce435510c0c5571
Anubhav <anubhav_a@me.iitr.ac.in>
2024-11-15 17:06:23 +0530

    updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
-    domains: ['images.unsplash.com', 'files.edgestore.dev'],
-    unoptimized: process.env.NODE_ENV === 'production',
+    domains: ["images.unsplash.com", "files.edgestore.dev"],
   },
-  distDir: '.next',
 };
 
 export default nextConfig;
